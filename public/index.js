35a975160bea9fffa632de7cf4c92b66620440da
Miko Babiano <miko.gaming1975@gmail.com>
2023-11-13 16:30:39 +0800

    Base code 2023-11-13

---
+++ public/index.js
@@ -0,0 +1,33 @@
+// DOM Manipulation and Event Handling
+
+/** 1. QUERY SELECTOR */
+    // query selector
+    // getElementByID
+    // getElementsByClass
+    // getElementsByTag
+    // query selector All
+
+/** 2. ACCESSING HTML CONTENT */
+    //innerHTML
+    //innerText
+    //textContent
+
+/** 3. ATTRIBUTE MANIPULATION */
+    //getAttribute
+    //setAttribute
+    //removeAttribute
+    //property attribute
+    //dataset property
+
+/** 4. CLASS MANIPULATION */
+    //classList vs className
+    //.add()
+    //.remove()
+    //.toggle()
+    //.contains
+
+/** 5. ELEMENT MANIPULATION */
+    //Literal syntax
+    //createElement
+    
+/** 6. EVENT HANDLING */
